873af0b0997435ad611e14b5160804e268404c9b
saichandudathrika <dathrikasaichandu24@gmail.com>
2022-03-09 10:05:10 -0800

    java ref code

---
+++ JavaTheCompletereferenceBook/src/anOverviewaOfJava/Example2.java
@@ -0,0 +1,13 @@
+package anOverviewaOfJava;
+
+public class Example2 {
+	public static void main(String[] args) {
+		int num;
+		num = 100;
+		System.out.println("Num : "+ num);
+		num = num*2;
+		System.out.println("num * 2 is :" + num);
+		
+	}
+
+}
